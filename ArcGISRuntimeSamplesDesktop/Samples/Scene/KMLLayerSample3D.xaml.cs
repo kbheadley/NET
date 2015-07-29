ff9555a89d74c09763697a3c3cc7042833890e1c
kbheadley <kbhsubscribe@gmail.com>
2015-07-29 13:00:14 -0400

    comment

---
+++ ArcGISRuntimeSamplesDesktop/Samples/Scene/KMLLayerSample3D.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace ArcGISRuntime.Samples.Desktop
+{
+	/// <summary>
+	/// This sample demonstrates adding a KML to a Scene in XAML.
+	/// </summary>
+	/// <title>3D KML Layer</title>
+	/// <category>Scene</category>
+	/// <subcategory>Layers</subcategory>
+	public partial class KMLLayerSample3D : UserControl
+	{
+		public KMLLayerSample3D()
+		{
+			InitializeComponent();
+		}
+	}
+}
